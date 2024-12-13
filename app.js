6ed7a486a1334550435c74cceeb3240a839ca129
Rumesh36 <rrumesh54@email.com>
2024-12-13 23:48:54 +0530

    home page

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function boxhide(){
     const box=document.querySelectorAll(".box");  
     const t1=document.querySelector(".text1");  
     const t2=document.querySelector(".text2");
-    boxes.forEach(box => box.style.display = "none");
+    box.style.display = "none";
     t1.style.display="none";
     t2.style.display="inline";  
 }
