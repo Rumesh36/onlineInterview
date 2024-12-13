37a2f3714068173bfe35ae4f8c68c8b71ea30889
Rumesh36 <rrumesh54@email.com>
2024-12-14 00:06:51 +0530

    home page

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ function boxhide(){
     const boxes=document.querySelectorAll(".box");  
     const t1=document.querySelector(".text1");  
     const t2=document.querySelector(".text2");
+    const p=document.querySelector(".domain");
     boxes.forEach( boxes=> {
         boxes.style.display="none";
     });
     t1.style.display="none";
     t2.style.display="inline";  
+    p.innerHTML=boxes;
 }
